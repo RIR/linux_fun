4c3e0e1ed4947084cd397a9e97b1a41494bed6e8
Student Laptop User <fuksi@dhcp-asv-103.helsinki.fi>
2016-12-19 19:04:39 +0200

    imgur valmis

---
+++ Week6/hipstafy-url.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script hipsterizes a downloaded image and uploads it to imgur.
+ . ticktick.sh
+
+client_id="4879e067a5136df"
+curl -O $1
+inputfile=$(basename $1)
+if [[ $inputfile == *.jpg ]]
+then  
+prefix=${inputfile%.jpg}
+outputfile=$prefix-hipstah.jpg
+convert -sepia-tone 60% +polaroid $inputfile $outputfile
+fi
+
+data=$(curl -v -H "Content-Type: application/json" -H "Authorization: Client-ID $client_id" --data-binary "@$outputfile" https://api.imgur.com/3/upload)
+
+tickParse "$data"
+echo "Your hipstafied image is located in ``data.link``"
+
